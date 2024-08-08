dfc1b26c1592a385649b9bd39b768d1b8f247e48
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-08 14:56:35 +0900

    [17] 스키마, 모델 만들기

---
+++ myContacts/models/contactModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema( {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String,
+        required: [true, "전화번호는 꼭 입력해주세요."]
+    }
+},
+{
+    timestamps: true
+})
+
+// schema -> model 만들기
+// mongoose.model(모델명, 스키마명)
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
